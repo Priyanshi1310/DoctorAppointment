ea4777db93102e5f01b6183f44ccaf171fe1a5c2
Priyanshi1310 <priyanshi13gupta@gmail.com>
2025-02-14 18:53:13 +0530

    react_base_url

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,4 @@
-export const BASE_URL = "http://localhost:3000/api/v1";
+const react_base_url = import.meta.env.REACT_APP_BASE_URL;
+
+export const BASE_URL = react_base_url || "http://localhost:3000/api/v1";
 export const token = localStorage.getItem('token')
